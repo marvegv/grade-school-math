c84a9a4502745a8f94e5efc441d66d037c092c17
marvegv <45470831+marvegv@users.noreply.github.com>
2023-08-12 21:54:33 +0200

    Update train.py

--- grade_school_math/train.py
+++ grade_school_math/train.py
@@ -5,6 +5,7 @@ from transformers import GPT2Config, AdamW
 from transformers import get_scheduler
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 
 def main():
